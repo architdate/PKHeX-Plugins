179908e71741808477861e58834605926ba022b8
Archit Date <architdate@gmail.com>
2018-07-15 03:06:09 +0800

    Bruteforce: geo location as interface
    fixes issue caused by https://github.com/kwsch/PKHeX/commit/625a77589b4b004c6479b993a999e719024f8a00

--- AutoLegalityMod/PKSMAutoLegality.cs
+++ AutoLegalityMod/PKSMAutoLegality.cs
@@ -948,7 +948,8 @@ namespace AutoLegalityMod
             }
             if (report.Contains(V146))
             {
-                pk.Geo1_Country = 1; // Prev residence
+                var g = (IGeoTrack) pk;
+                g.Geo1_Country = 1;
                 report = UpdateReport(pk);
             }
             if (report.Contains(V150)) //V150 = Memory: Handling Trainer Memory missing.
@@ -991,7 +992,8 @@ namespace AutoLegalityMod
             }
             if (report.Contains(V137)) //V137 = GeoLocation Memory: Memories should be present.
             {
-                pk.Geo1_Country = 1;
+                var g = (IGeoTrack) pk;
+                g.Geo1_Country = 1;
                 report = UpdateReport(pk);
             }
             if (report.Contains(V118)) //V118 = Can't have ball for encounter type.
