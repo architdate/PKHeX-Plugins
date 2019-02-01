72ae5828f85eff46b458a7c1a56b9132ff2bad17
Kurt <kaphotics@gmail.com>
2019-01-31 21:40:00 -0800

    Add pastebin set test

--- AutoModTests/WebSetFetch.cs
+++ AutoModTests/WebSetFetch.cs
@@ -27,8 +27,22 @@ namespace AutoModTests
         }
 
         [Theory]
-        [InlineData("https://pokepast.es/73c130c81caab03e", "STING LIKE A BEE", 15, 801)]
+        [InlineData("https://pokepast.es/73c130c81caab03e", "STING LIKE A BEE", 15, 801)] // Beedrill, Magearna
         public void HasPokePasteSets(string url, string name, params int[] speciesPresent)
+        {
+            var tpi = new TeamPasteInfo(url);
+            tpi.Valid.Should().BeTrue();
+            tpi.Title.Should().Be(name);
+
+            var team = ShowdownUtil.ShowdownSets(tpi.Sets);
+            var species = team.Select(s => s.Species).ToList();
+            var hasAll = speciesPresent.All(species.Contains);
+            hasAll.Should().BeTrue();
+        }
+
+        [Theory]
+        [InlineData("https://pastebin.com/0x7jJvB4", "Untitled", 241, 628)] // Miltank...Braviary
+        public void HasPastebinSets(string url, string name, params int[] speciesPresent)
         {
             var tpi = new TeamPasteInfo(url);
             tpi.Valid.Should().BeTrue();
