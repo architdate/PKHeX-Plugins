eb600bd39f8971ce11bddd44188e5e307282d160
Archit Date <architdate@gmail.com>
2019-02-13 12:28:51 +0800

    plugin: add plugin to export PKSM box to PKHeX

---
+++ AutoLegalityMod/Plugins/ExportPKSMBank.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace AutoModPlugins
+{
+    public class ExportPKSMBank : AutoModPlugin
+    {
+        public override string Name => "Export PKSM Bank to PKM";
+        public override int Priority => 1;
+
+        protected override void AddPluginControl(ToolStripDropDownItem modmenu)
+        {
+            var ctrl = new ToolStripMenuItem(Name);
+            modmenu.DropDownItems.Add(ctrl);
+            ctrl.Click += Export;
+            ctrl.Image = Properties.Resources.mgdbdownload;
+        }
+
+        private void Export(object sender, EventArgs e)
+        {
+            var bank = PKSMUtil.GetBankData();
+
+            // Check for invalid bank
+            if (bank == null)
+            {
+                WinFormsUtil.Alert("Invalid bank input");
+                return;
+            }
+
+            PKSMUtil.ExportBank(bank);
+            WinFormsUtil.Alert("Bank Exported");
+        }
+    }
+}
