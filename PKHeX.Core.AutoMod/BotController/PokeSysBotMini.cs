9c1ec49ae818da9317f734dd90e800f85f23ef38
Archit Date <architdate@gmail.com>
2020-02-01 14:32:17 +0800

    [LiveHeX] fix writebox
    single writes work. why? who knows.

--- PKHeX.Core.AutoMod/BotController/PokeSysBotMini.cs
+++ PKHeX.Core.AutoMod/BotController/PokeSysBotMini.cs
@@ -1,4 +1,6 @@
-﻿namespace PKHeX.Core.AutoMod
+﻿using System;
+
+namespace PKHeX.Core.AutoMod
 {
     public class PokeSysBotMini : SysBotMini
     {
@@ -12,7 +14,25 @@
         public byte[] ReadBox(int box, int len) => ReadBytes(GetBoxOffset(box), len);
         public byte[] ReadSlot(int box, int slot) => ReadBytes(GetSlotOffset(box, slot), 344);
 
-        public void SendBox(byte[] boxData, int box) => WriteBytes(boxData, GetBoxOffset(box));
+        public void SendBox(byte[] boxData, int box)
+        {
+            byte[][] pkmData = BufferSplit(boxData, SlotSize);
+            for (int i = 0; i < SlotCount; i++)
+                SendSlot(pkmData[i], box, i);
+        }
         public void SendSlot(byte[] data, int box, int slot) => WriteBytes(data, GetSlotOffset(box, slot));
+
+        public static byte[][] BufferSplit(byte[] buffer, int blockSize)
+        {
+            byte[][] blocks = new byte[(buffer.Length + blockSize - 1) / blockSize][];
+
+            for (int i = 0, j = 0; i < blocks.Length; i++, j += blockSize)
+            {
+                blocks[i] = new byte[Math.Min(blockSize, buffer.Length - j)];
+                Array.Copy(buffer, j, blocks[i], 0, blocks[i].Length);
+            }
+
+            return blocks;
+        }
     }
 }
