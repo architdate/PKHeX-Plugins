4d964ee083453846824e69b3cd178c5416cdb978
Archit Date <architdate@gmail.com>
2020-07-29 19:32:05 +0800

    add functionality to create pksm bank
    also update dump functionality for v1, v2 and v3 pksm banks

---
+++ AutoLegalityMod/Plugins/PKSMBankPlugin.cs
@@ -0,0 +1,53 @@
+﻿using System.IO;
+using System.Windows.Forms;
+using PKHeX.Core.AutoMod;
+
+namespace AutoModPlugins
+{
+    public class PKSMBankPlugin : AutoModPlugin
+    {
+        public override string Name => "PKSM Bank Tools";
+        public override int Priority => 2;
+
+        protected override void AddPluginControl(ToolStripDropDownItem modmenu)
+        {
+            var ctrl = new ToolStripMenuItem(Name) {Name = "Menu_PKSMBank"};
+
+            var c1 = new ToolStripMenuItem("Merge PKM into PKSM Bank") { Image = Properties.Resources.uploadgpss };
+            c1.Click += (s, e) => Import(); 
+            c1.Name = "Menu_CreatePKSMBank";
+            var c2 = new ToolStripMenuItem("Split PKSM Bank into PKM") { Image = Properties.Resources.mgdbdownload };
+            c2.Click += (s, e) => Export();
+            c2.Name = "Menu_ExportPKSMBank";
+
+            ctrl.DropDownItems.Add(c1);
+            ctrl.DropDownItems.Add(c2);
+            modmenu.DropDownItems.Add(ctrl);
+            
+            ctrl.Image = Properties.Resources.flagbrew;
+        }
+
+        private static void Export()
+        {
+            if (!WinFormsUtil.OpenSAVPKMDialog(new[] { ".bnk" }, out string path))
+                return;
+
+            var bank = File.ReadAllBytes(path);
+            using var fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() != DialogResult.OK)
+                return;
+            var count = PKSMUtil.ExportBank(bank, fbd.SelectedPath, out var previews);
+            PKMPreview.ExportCSV(previews, fbd.SelectedPath);
+            WinFormsUtil.Alert("Bank Exported!", $"Dumped {count} Pokémon!");
+        }
+
+        private static void Import()
+        {
+            using var fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() != DialogResult.OK)
+                return;
+            var count = PKSMUtil.CreateBank(fbd.SelectedPath);
+            WinFormsUtil.Alert("Bank Created!", $"Added {count} Pokémon to the bank!");
+        }
+    }
+}
--- PKHeX.Core.AutoMod/Enhancements/PKSMUtil.cs
+++ PKHeX.Core.AutoMod/Enhancements/PKSMUtil.cs
@@ -1,13 +1,61 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace PKHeX.Core.AutoMod
 {
     public static class PKSMUtil
     {
         /// <summary>
-        /// Exports PKSM's bank.bin to individual <see cref="PKM"/> files
+        /// Creates PKSM's bank.bin to individual <see cref="PKM"/> files (v1)
+        /// </summary>
+        /// <param name="bank">PKSM format bank storage</param>
+        /// <param name="dir">Folder to export all dumped files to</param>
+        /// <param name="previews">Preview data</param>
+        public static int CreateBank(string dir)
+        {
+            var files = Directory.GetFiles(dir, "*.p??", SearchOption.TopDirectoryOnly);
+            var ver = Enum.GetValues(typeof(PKSMBankVersion)).Cast<int>().Max();
+            var version = BitConverter.GetBytes(ver + 1); // Latest bank version
+            var pksmsize = GetBankSize((PKSMBankVersion) ver);
+            var boxcount = (files.Length / 30) + 1;
+            var bank = new byte[8 + 4 + 4 + boxcount * pksmsize * 30];
+            var ctr = 0;
+            var magic = new byte[] {0x50, 0x4B, 0x53, 0x4D, 0x42, 0x41, 0x4E, 0x4B}; // PKSMBANK
+            magic.CopyTo(bank, 0);
+            version.CopyTo(bank, 8);
+            BitConverter.GetBytes(boxcount).CopyTo(bank, 12); // Number of bank boxes.
+            foreach (var f in files)
+            {
+                var pk = PKMConverter.GetPKMfromBytes(File.ReadAllBytes(f));
+                if (pk == null)
+                    continue;
+                if (pk.Species == 0 && pk.Species >= pk.MaxSpeciesID)
+                    continue;
+                var ofs = 16 + ctr * pksmsize;
+                BitConverter.GetBytes((int) GetPKSMFormat(pk)).CopyTo(bank, ofs);
+                pk.DecryptedBoxData.CopyTo(bank, ofs + 4);
+                byte[] temp = Enumerable.Repeat((byte)0xFF, pksmsize - pk.DecryptedBoxData.Length - 8).ToArray();
+                temp.CopyTo(bank, ofs + pk.DecryptedBoxData.Length + 4);
+                temp = Enumerable.Repeat((byte)0x00, 4).ToArray();
+                temp.CopyTo(bank, ofs + pksmsize - 4);
+                ctr++;
+            }
+            var empty = (boxcount * 30) - files.Length;
+            for (int i = 0; i < empty; i++)
+            {
+                var ofs = 16 + ctr * pksmsize;
+                byte[] temp = Enumerable.Repeat((byte)0xFF, pksmsize).ToArray();
+                temp.CopyTo(bank, ofs);
+                ctr++;
+            }
+            File.WriteAllBytes(Path.Combine(dir, "alm.bnk"), bank);
+            return ctr - empty;
+        }
+
+        /// <summary>
+        /// Exports PKSM's bank.bin to individual <see cref="PKM"/> files (v1)
         /// </summary>
         /// <param name="bank">PKSM format bank storage</param>
         /// <param name="dir">Folder to export all dumped files to</param>
@@ -16,8 +64,12 @@ namespace PKHeX.Core.AutoMod
         {
             Directory.CreateDirectory(dir);
             var ctr = 0;
+            var version = BitConverter.ToUInt32(bank, 8);
+            var ver = (PKSMBankVersion)(version & 0xFF);
+            var pkmsize = GetBankSize(ver);
+            var start = GetBankStartIndex(ver);
             previews = new List<PKMPreview>();
-            for (int i = 12; i < bank.Length; i += 4 + 260)
+            for (int i = start; i < bank.Length; i += pkmsize)
             {
                 var pk = GetPKSMStoredPKM(bank, i);
                 if (pk == null)
@@ -46,11 +98,15 @@ namespace PKHeX.Core.AutoMod
 
             return format switch
             {
-                PKSMStorageFormat.FOUR => (PKM)new PK4(Slice(data, ofs + 4, 136)),
+                PKSMStorageFormat.ONE => new PK1(Slice(data, ofs + 4, 33)),
+                PKSMStorageFormat.TWO => new PK2(Slice(data, ofs + 4, 32)),
+                PKSMStorageFormat.THREE => new PK3(Slice(data, ofs + 4, 80)),
+                PKSMStorageFormat.FOUR => new PK4(Slice(data, ofs + 4, 136)),
                 PKSMStorageFormat.FIVE => new PK5(Slice(data, ofs + 4, 136)),
                 PKSMStorageFormat.SIX => new PK6(Slice(data, ofs + 4, 232)),
                 PKSMStorageFormat.SEVEN => new PK7(Slice(data, ofs + 4, 232)),
                 PKSMStorageFormat.LGPE => new PB7(Slice(data, ofs + 4, 232)),
+                PKSMStorageFormat.EIGHT => new PK8(Slice(data, ofs + 4, 328)),
                 _ => null
             };
         }
@@ -70,6 +126,49 @@ namespace PKHeX.Core.AutoMod
             return 0 != BitConverter.ToUInt16(data, offset + 8);
         }
 
+        private static int GetBankSize(PKSMBankVersion v)
+        {
+            return v switch
+            {
+                PKSMBankVersion.VERSION1 => 264,
+                PKSMBankVersion.VERSION2 => 264,
+                PKSMBankVersion.VERSION3 => 336,
+                _ => 336
+            };
+        }
+
+        private static int GetBankStartIndex(PKSMBankVersion v)
+        {
+            return v switch
+            {
+                PKSMBankVersion.VERSION1 => 12,
+                PKSMBankVersion.VERSION2 => 16,
+                PKSMBankVersion.VERSION3 => 16,
+                _ => 16
+            };
+        }
+
+        private static PKSMStorageFormat GetPKSMFormat(PKM pk)
+        {
+            if (pk is PK1 _) return PKSMStorageFormat.ONE;
+            if (pk is PK2 _) return PKSMStorageFormat.TWO;
+            if (pk is PK3 _) return PKSMStorageFormat.THREE;
+            if (pk is PK4 _) return PKSMStorageFormat.FOUR;
+            if (pk is PK5 _) return PKSMStorageFormat.FIVE;
+            if (pk is PK6 _) return PKSMStorageFormat.SIX;
+            if (pk is PK7 _) return PKSMStorageFormat.SEVEN;
+            if (pk is PB7 _) return PKSMStorageFormat.LGPE;
+            if (pk is PK8 _) return PKSMStorageFormat.EIGHT;
+            return PKSMStorageFormat.UNUSED;
+        }
+
+        private enum PKSMBankVersion
+        {
+            VERSION1,
+            VERSION2,
+            VERSION3,
+        }
+
         private enum PKSMStorageFormat
         {
             FOUR,
@@ -77,6 +176,10 @@ namespace PKHeX.Core.AutoMod
             SIX,
             SEVEN,
             LGPE,
+            EIGHT,
+            ONE,
+            TWO,
+            THREE,
             MAX_COUNT,
             UNUSED = 0xFF
         }
