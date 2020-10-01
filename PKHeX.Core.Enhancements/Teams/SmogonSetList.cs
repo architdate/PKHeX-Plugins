34c0a89610eaec0bbf0f873d18d37687f4f467f0
soopercool101 <soopercoolstages@gmail.com>
2020-10-01 00:58:07 -0700

    Smogon set importer now respects hidden power types (#59)

--- PKHeX.Core.Enhancements/Teams/SmogonSetList.cs
+++ PKHeX.Core.Enhancements/Teams/SmogonSetList.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -213,6 +213,8 @@ namespace PKHeX.Core.Enhancements
                     var move = GetMove(choice);
                     if (moves.Contains(move))
                         continue;
+                    if (move.Equals("Hidden Power", StringComparison.OrdinalIgnoreCase))
+                        move = $"{move} [{choice.Split(new[] {"\"type\":\""}, StringSplitOptions.None)[1].Split(new [] {'\"'})[0]}]";
                     moves.Add(move);
                     break;
                 }
