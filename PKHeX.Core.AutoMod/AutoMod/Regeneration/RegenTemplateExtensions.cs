de54289fd187ded8265aafb370f92682166148f9
Archit Date <architdate@gmail.com>
2020-10-22 16:50:03 +0800

    add a todo for battle specific moves
    such as zacians behemoth blade

--- PKHeX.Core.AutoMod/AutoMod/Regeneration/RegenTemplateExtensions.cs
+++ PKHeX.Core.AutoMod/AutoMod/Regeneration/RegenTemplateExtensions.cs
@@ -20,6 +20,7 @@ namespace PKHeX.Core.AutoMod
         /// Showdown quirks lets you have battle only moves in battle only formes. Transform back to base move.
         /// </summary>
         /// <param name="set"></param>
+        /// TODO: Sanitize Battle specific moves for calyrex??
         public static void SanitizeBattleMoves(this IBattleTemplate set)
         {
             switch (set.Species)
