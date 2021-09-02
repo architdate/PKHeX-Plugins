457fd86c62b2cfdaa21db9c3fd9f4f1458741875
Koi <koi.github3088@gmail.com>
2021-09-02 03:40:52 -0500

    Inherit the ICommunicatorNX interface for UsbBotMini for pointer reading relative to heap, streamline encoding via an optional boolean.

--- PKHeX.Core.Injection/BotController/SwitchCommand.cs
+++ PKHeX.Core.Injection/BotController/SwitchCommand.cs
@@ -14,8 +14,9 @@ namespace PKHeX.Core.Injection
         /// <summary>
         /// Removes the virtual controller from the bot. Allows physical controllers to control manually.
         /// </summary>
+        /// <param name="addrn">Encoding selector. Default "true" for sys-botbase.</param>
         /// <returns>Encoded command bytes</returns>
-        public static byte[] DetachController() => Encode("detachController");
+        public static byte[] DetachController(bool addrn = true) => Encode("detachController", addrn);
 
         /* 
          *
@@ -27,23 +28,26 @@ namespace PKHeX.Core.Injection
         /// Presses and releases a <see cref="SwitchButton"/> for 50ms.
         /// </summary>
         /// <param name="button">Button to click.</param>
+        /// <param name="addrn">Encoding selector. Default "true" for sys-botbase.</param>
         /// <remarks>Press &amp; Release timing is performed by the console automatically.</remarks>
         /// <returns>Encoded command bytes</returns>
-        public static byte[] Click(SwitchButton button) => Encode($"click {button}");
+        public static byte[] Click(SwitchButton button, bool addrn = true) => Encode($"click {button}", addrn);
 
         /// <summary>
         /// Presses and does NOT release a <see cref="SwitchButton"/>.
         /// </summary>
         /// <param name="button">Button to hold.</param>
+        /// <param name="addrn">Encoding selector. Default "true" for sys-botbase.</param>
         /// <returns>Encoded command bytes</returns>
-        public static byte[] Hold(SwitchButton button) => Encode($"press {button}");
+        public static byte[] Hold(SwitchButton button, bool addrn = true) => Encode($"press {button}", addrn);
 
         /// <summary>
         /// Releases the held <see cref="SwitchButton"/>.
         /// </summary>
         /// <param name="button">Button to release.</param>
+        /// <param name="addrn">Encoding selector. Default "true" for sys-botbase.</param>
         /// <returns>Encoded command bytes</returns>
-        public static byte[] Release(SwitchButton button) => Encode($"release {button}");
+        public static byte[] Release(SwitchButton button, bool addrn = true) => Encode($"release {button}", addrn);
 
         /* 
          *
@@ -54,14 +58,20 @@ namespace PKHeX.Core.Injection
         /// <summary>
         /// Sets the specified <see cref="stick"/> to the desired <see cref="x"/> and <see cref="y"/> positions.
         /// </summary>
+        /// <param name="stick">LEFT or RIGHT joystick enumerator.</param>
+        /// <param name="x">X axis magnitude.</param>
+        /// <param name="y">Y axis magnitude.</param>
+        /// <param name="addrn">Encoding selector. Default "true" for sys-botbase.</param>
         /// <returns>Encoded command bytes</returns>
-        public static byte[] SetStick(SwitchStick stick, int x, int y) => Encode($"setStick {stick} {x} {y}");
+        public static byte[] SetStick(SwitchStick stick, int x, int y, bool addrn = true) => Encode($"setStick {stick} {x} {y}", addrn);
 
         /// <summary>
         /// Resets the specified <see cref="stick"/> to (0,0)
         /// </summary>
+        /// <param name="stick">LEFT or RIGHT joystick enumerator.</param>
+        /// <param name="addrn">Encoding selector. Default "true" for sys-botbase.</param>
         /// <returns>Encoded command bytes</returns>
-        public static byte[] ResetStick(SwitchStick stick) => SetStick(stick, 0, 0);
+        public static byte[] ResetStick(SwitchStick stick, bool addrn = true) => SetStick(stick, 0, 0, addrn);
 
         /* 
          *
@@ -74,69 +84,60 @@ namespace PKHeX.Core.Injection
         /// </summary>
         /// <param name="offset">Address of the data</param>
         /// <param name="count">Amount of bytes</param>
+        /// <param name="addrn">Encoding selector. Default "true" for sys-botbase.</param>
         /// <returns>Encoded command bytes</returns>
-        public static byte[] Peek(uint offset, int count) => Encode($"peek 0x{offset:X8} {count}");
+        public static byte[] Peek(uint offset, int count, bool addrn = true) => Encode($"peek 0x{offset:X8} {count}", addrn);
 
         /// <summary>
         /// Sends the Bot <see cref="data"/> to be written to <see cref="offset"/>.
         /// </summary>
         /// <param name="offset">Address of the data</param>
         /// <param name="data">Data to write</param>
+        /// <param name="addrn">Encoding selector. Default "true" for sys-botbase.</param>
         /// <returns>Encoded command bytes</returns>
-        public static byte[] Poke(uint offset, byte[] data) => Encode($"poke 0x{offset:X8} 0x{string.Concat(data.Select(z => $"{z:X2}"))}");
-
-        /// <summary>
-        /// (Without return characters for USB-Botbase) Requests the Bot to send <see cref="count"/> bytes from <see cref="offset"/>.
-        /// </summary>
-        /// <param name="offset">Address of the data</param>
-        /// <param name="count">Amount of bytes</param>
-        /// <returns>Encoded command bytes</returns>
-        public static byte[] PeekRaw(uint offset, int count) => Encode($"peek 0x{offset:X8} {count}", false);
-
-        /// <summary>
-        /// (Without return characters for USB-Botbase) Sends the Bot <see cref="data"/> to be written to <see cref="offset"/>.
-        /// </summary>
-        /// <param name="offset">Address of the data</param>
-        /// <param name="data">Data to write</param>
-        /// <returns>Encoded command bytes</returns>
-        public static byte[] PokeRaw(uint offset, byte[] data) => Encode($"poke 0x{offset:X8} 0x{string.Concat(data.Select(z => $"{z:X2}"))}", false);
+        public static byte[] Poke(uint offset, byte[] data, bool addrn = true) => Encode($"poke 0x{offset:X8} 0x{string.Concat(data.Select(z => $"{z:X2}"))}", addrn);
 
         /// <summary>
         /// Requests the Bot to send <see cref="count"/> bytes from absolute <see cref="offset"/>.
         /// </summary>
         /// <param name="offset">Absolute address of the data</param>
         /// <param name="count">Amount of bytes</param>
+        /// <param name="addrn">Encoding selector. Default "true" for sys-botbase.</param>
         /// <returns>Encoded command bytes</returns>
-        public static byte[] PeekAbsolute(ulong offset, int count) => Encode($"peekAbsolute 0x{offset:X16} {count}");
+        public static byte[] PeekAbsolute(ulong offset, int count, bool addrn = true) => Encode($"peekAbsolute 0x{offset:X16} {count}", addrn);
 
         /// <summary>
         /// Sends the Bot <see cref="data"/> to be written to absolute <see cref="offset"/>.
         /// </summary>
         /// <param name="offset">Absolute address of the data</param>
         /// <param name="data">Data to write</param>
+        /// <param name="addrn">Encoding selector. Default "true" for sys-botbase.</param>
         /// <returns>Encoded command bytes</returns>
-        public static byte[] PokeAbsolute(ulong offset, byte[] data) => Encode($"pokeAbsolute 0x{offset:X16} 0x{string.Concat(data.Select(z => $"{z:X2}"))}");
+        public static byte[] PokeAbsolute(ulong offset, byte[] data, bool addrn = true) => Encode($"pokeAbsolute 0x{offset:X16} 0x{string.Concat(data.Select(z => $"{z:X2}"))}", addrn);
 
         /// <summary>
         /// Requests the Bot to send <see cref="count"/> bytes from main <see cref="offset"/>.
         /// </summary>
         /// <param name="offset">Address of the data relative to main</param>
         /// <param name="count">Amount of bytes</param>
+        /// <param name="addrn">Encoding selector. Default "true" for sys-botbase.</param>
         /// <returns>Encoded command bytes</returns>
-        public static byte[] PeekMain(ulong offset, int count) => Encode($"peekMain 0x{offset:X16} {count}");
+        public static byte[] PeekMain(ulong offset, int count, bool addrn = true) => Encode($"peekMain 0x{offset:X16} {count}", addrn);
 
         /// <summary>
         /// Sends the Bot <see cref="data"/> to be written to main <see cref="offset"/>.
         /// </summary>
         /// <param name="offset">Address of the data relative to main</param>
         /// <param name="data">Data to write</param>
+        /// <param name="addrn">Encoding selector. Default "true" for sys-botbase.</param>
         /// <returns>Encoded command bytes</returns>
-        public static byte[] PokeMain(ulong offset, byte[] data) => Encode($"pokeMain 0x{offset:X16} 0x{string.Concat(data.Select(z => $"{z:X2}"))}");
+        public static byte[] PokeMain(ulong offset, byte[] data, bool addrn = true) => Encode($"pokeMain 0x{offset:X16} 0x{string.Concat(data.Select(z => $"{z:X2}"))}", addrn);
 
         /// <summary>
         /// Requests the Bot to send the address of the Heap base.
         /// </summary>
+        /// <param name="addrn">Encoding selector. Default "true" for sys-botbase.</param>
         /// <returns>Encoded command bytes</returns>
-        public static byte[] GetHeapBase() => Encode($"getHeapBase");
+        public static byte[] GetHeapBase(bool addrn = true) => Encode($"getHeapBase", addrn);
     }
 }
