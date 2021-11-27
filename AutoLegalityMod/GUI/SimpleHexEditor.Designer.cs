ff84dccc94daa9c0729dbbc272121f1dc9343893
Archit Date <architdate@gmail.com>
2021-11-28 04:55:22 +0800

    added fody and costura for dll merging
    update ci/cd

--- AutoLegalityMod/GUI/SimpleHexEditor.Designer.cs
+++ AutoLegalityMod/GUI/SimpleHexEditor.Designer.cs
@@ -31,25 +31,12 @@ namespace AutoModPlugins.GUI
         /// </summary>
         private void InitializeComponent()
         {
-            this.RTB_RAM = new AutoModPlugins.GUI.HexRichTextBox();
             this.B_Update = new System.Windows.Forms.Button();
             this.PG_BlockView = new System.Windows.Forms.PropertyGrid();
             this.CB_AutoRefresh = new System.Windows.Forms.CheckBox();
             this.CB_CopyMethod = new System.Windows.Forms.ComboBox();
+            this.RTB_RAM = new AutoModPlugins.GUI.HexRichTextBox();
             this.SuspendLayout();
-            // 
-            // RTB_RAM
-            // 
-            this.RTB_RAM.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.RTB_RAM.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.RTB_RAM.Location = new System.Drawing.Point(9, 10);
-            this.RTB_RAM.Name = "RTB_RAM";
-            this.RTB_RAM.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedVertical;
-            this.RTB_RAM.Size = new System.Drawing.Size(364, 292);
-            this.RTB_RAM.TabIndex = 0;
-            this.RTB_RAM.Text = "";
             // 
             // B_Update
             // 
@@ -89,12 +76,27 @@ namespace AutoModPlugins.GUI
             // 
             // CB_CopyMethod
             // 
+            this.CB_CopyMethod.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.CB_CopyMethod.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.CB_CopyMethod.FormattingEnabled = true;
             this.CB_CopyMethod.Location = new System.Drawing.Point(145, 312);
             this.CB_CopyMethod.Name = "CB_CopyMethod";
             this.CB_CopyMethod.Size = new System.Drawing.Size(116, 21);
             this.CB_CopyMethod.TabIndex = 17;
             this.CB_CopyMethod.SelectedIndexChanged += new System.EventHandler(this.ChangeCopyMethod);
+            // 
+            // RTB_RAM
+            // 
+            this.RTB_RAM.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.RTB_RAM.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RTB_RAM.Location = new System.Drawing.Point(9, 10);
+            this.RTB_RAM.Name = "RTB_RAM";
+            this.RTB_RAM.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedVertical;
+            this.RTB_RAM.Size = new System.Drawing.Size(364, 292);
+            this.RTB_RAM.TabIndex = 0;
+            this.RTB_RAM.Text = "";
             // 
             // SimpleHexEditor
             // 
