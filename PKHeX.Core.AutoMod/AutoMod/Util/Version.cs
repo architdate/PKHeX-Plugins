6b47b9f229647ab700da3c8e2032ff192ea956c8
Archit Date <architdate@gmail.com>
2022-02-13 17:44:50 +0800

    update nuget and prepare for release

--- PKHeX.Core.AutoMod/AutoMod/Util/Version.cs
+++ PKHeX.Core.AutoMod/AutoMod/Util/Version.cs
@@ -3,7 +3,7 @@
     public static class ALMVersion
 
     {
-        public const string CurrentVersion = "22.02.08";
+        public const string CurrentVersion = "22.02.14";
 
         /*
          * TODO: Add other versioning code, maybe compatability lists here?
