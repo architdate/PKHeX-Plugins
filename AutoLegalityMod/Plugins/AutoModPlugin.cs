35a61b049489a04c5fb15b97403e0cb9cb78df4e
Kurt <kaphotics@gmail.com>
2022-03-02 19:26:39 -0800

    Defer form translation to post-init, only once.

--- AutoLegalityMod/Plugins/AutoModPlugin.cs
+++ AutoLegalityMod/Plugins/AutoModPlugin.cs
@@ -20,6 +20,7 @@ namespace AutoModPlugins
         private const string ParentMenuName = "Menu_AutoLegality";
         private const string ParentMenuText = "Auto-Legality Mod";
         private const string ParentMenuParent = "Menu_Tools";
+        private const string LoggingPrefix = "[Auto-Legality Mod]";
 
         public bool PossibleVersionMismatch;
 
@@ -35,46 +36,66 @@ namespace AutoModPlugins
 
         public void Initialize(params object[] args)
         {
-            Debug.WriteLine($"[Auto-Legality Mod] Loading {Name}");
+            Debug.WriteLine($"{LoggingPrefix} Loading {Name}");
             SaveFileEditor = (ISaveFileProvider)Array.Find(args, z => z is ISaveFileProvider);
             PKMEditor = (IPKMView)Array.Find(args, z => z is IPKMView);
             var menu = (ToolStrip)Array.Find(args, z => z is ToolStrip);
             LoadMenuStrip(menu);
-            // ReSharper disable once SuspiciousTypeConversion.Global
-            ((ContainerControl)SaveFileEditor).ParentForm.TranslateInterface(WinFormsTranslator.CurrentLanguage);
 
             // Match PKHeX Versioning and ALM Settings only on parent plugin
             if (Priority != 0)
                 return;
 
+            Task.Run(SetUpEnvironment);
+        }
+
+        private async Task SetUpEnvironment()
+        {
             ShowdownSetLoader.SetAPILegalitySettings();
+            await TranslateInterface().ConfigureAwait(false);
             CheckVersionUpdates();
+        }
+
+        private async Task TranslateInterface()
+        {
+            // ReSharper disable once SuspiciousTypeConversion.Global
+            var form = ((ContainerControl)SaveFileEditor).ParentForm;
+            if (form is null)
+                return;
+
+            // wait for all plugins to be loaded
+            while (!form.IsHandleCreated)
+                await Task.Delay(1_000).ConfigureAwait(false);
+
+            await Task.Delay(3_000).ConfigureAwait(false);
+            form.Invoke(() => form.TranslateInterface(WinFormsTranslator.CurrentLanguage));
+            Debug.WriteLine($"{LoggingPrefix} Translated form.");
         }
 
         private void CheckVersionUpdates()
         {
-            Task.Run(async () => {
-                var latest_alm = PKHeX.Core.AutoMod.NetUtil.GetLatestALMVersion();
-                var curr_valid = Version.TryParse(VERSION, out var current_alm);
-                var curr_pkhex = Assembly.GetEntryAssembly()?.GetName().Version;
-                if (!curr_valid || curr_pkhex == null || latest_alm == null)
-                    return;
-                var msg = $"Update for ALM is available. Please download it from GitHub. The updated release is only compatible with PKHeX version: {latest_alm.Major}.{latest_alm.Minor}.{latest_alm.Build}.";
-                if (curr_pkhex > current_alm)
-                    PossibleVersionMismatch = true;
-                if (latest_alm > current_alm)
-                {
-                    msg = PossibleVersionMismatch ? msg + "\n\nThere is also a possible version mismatch between the current ALM version and current PKHeX version." : msg;
-                    const string redirect = "\n\nClick on the GitHub button to get the latest update for ALM.\nClick on the Discord button if you still require further assistance.";
-                    while (!((ContainerControl)SaveFileEditor).ParentForm.IsHandleCreated)
-                        await Task.Delay(2_000).ConfigureAwait(false);
-                    var res = WinFormsUtil.ALMError(msg + redirect);
-                    if (res == DialogResult.Yes)
-                        Process.Start("https://discord.gg/tDMvSRv");
-                    else if (res == DialogResult.No)
-                        Process.Start("https://github.com/architdate/PKHeX-Plugins/releases/latest");
-                }
-            });
+            var latest_alm = PKHeX.Core.AutoMod.NetUtil.GetLatestALMVersion();
+            var curr_valid = Version.TryParse(VERSION, out var current_alm);
+            var curr_pkhex = Assembly.GetEntryAssembly()?.GetName().Version;
+            if (!curr_valid || curr_pkhex == null || latest_alm == null)
+                return;
+
+            if (curr_pkhex > current_alm)
+                PossibleVersionMismatch = true;
+            if (latest_alm <= current_alm)
+                return;
+
+            // ReSharper disable once SuspiciousTypeConversion.Global
+            var msg = $"Update for ALM is available. Please download it from GitHub. The updated release is only compatible with PKHeX version: {latest_alm.Major}.{latest_alm.Minor}.{latest_alm.Build}.";
+            if (PossibleVersionMismatch)
+                msg += "\n\nThere is also a possible version mismatch between the current ALM version and current PKHeX version.";
+            const string redirect = "Click on the GitHub button to get the latest update for ALM.\nClick on the Discord button if you still require further assistance.";
+
+            var res = WinFormsUtil.ALMError(msg, redirect);
+            if (res == DialogResult.Yes)
+                Process.Start("https://discord.gg/tDMvSRv");
+            else if (res == DialogResult.No)
+                Process.Start("https://github.com/architdate/PKHeX-Plugins/releases/latest");
         }
 
         private void LoadMenuStrip(ToolStrip menuStrip)
