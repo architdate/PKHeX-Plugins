b19c85dc9cda8a71e2901b1268e977d12062df20
architdate <architdate@gmail.com>
2022-09-16 23:43:32 +0800

    bump version up and hotfix release
    closes #147

--- PKHeX.Core.AutoMod/AutoMod/Util/Version.cs
+++ PKHeX.Core.AutoMod/AutoMod/Util/Version.cs
@@ -3,7 +3,7 @@
     public static class ALMVersion
 
     {
-        public const string CurrentVersion = "22.08.31";
+        public const string CurrentVersion = "22.09.13";
 
         /*
          * TODO: Add other versioning code, maybe compatability lists here?
