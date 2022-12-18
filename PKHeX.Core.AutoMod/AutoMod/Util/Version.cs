8e0ba09bebf4656e10497e3e66f9044ade86e2f0
architdate <architdate@gmail.com>
2022-12-18 21:07:01 +0800

    update plugins to v 22.12.18

--- PKHeX.Core.AutoMod/AutoMod/Util/Version.cs
+++ PKHeX.Core.AutoMod/AutoMod/Util/Version.cs
@@ -3,7 +3,7 @@
     public static class ALMVersion
 
     {
-        public const string CurrentVersion = "22.12.01";
+        public const string CurrentVersion = "22.12.18";
 
         /*
          * TODO: Add other versioning code, maybe compatability lists here?
