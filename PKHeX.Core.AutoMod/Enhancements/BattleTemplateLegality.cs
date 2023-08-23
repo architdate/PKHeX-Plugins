6a3be2327ed38d2c22887dd784332a297f276d51
Archit Date <architdate@gmail.com>
2023-08-23 22:32:55 +0800

    test project updates

--- PKHeX.Core.AutoMod/Enhancements/BattleTemplateLegality.cs
+++ PKHeX.Core.AutoMod/Enhancements/BattleTemplateLegality.cs
@@ -100,7 +100,7 @@ namespace PKHeX.Core.AutoMod
 
             // Ability checks
             var abilityreq = APILegality.GetRequestedAbility(failed, set);
-            if (abilityreq == AbilityRequest.NotHidden && encounters.All(z => z is EncounterStatic { Ability: AbilityPermission.OnlyHidden }))
+            if (abilityreq == AbilityRequest.NotHidden && encounters.All(z => z is IEncounterable { Ability: AbilityPermission.OnlyHidden }))
                 return string.Format(ONLY_HIDDEN_ABILITY_AVAILABLE, species_name);
             if (abilityreq == AbilityRequest.Hidden && encounters.All(z => z.Generation is 3 or 4) && destVer.GetGeneration() < 8)
                 return string.Format(HIDDEN_ABILITY_UNAVAILABLE, species_name);
